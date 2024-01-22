bca0a5404af7b637f7c2b4b97c8d8a0e20f610c1
RenanCruz <renancruz199@gmail.com>
2024-01-22 19:25:08 -0300

    Observables e Interface

--- angular-rxjs/src/app/views/lista-livros/lista-livros.component.ts
+++ angular-rxjs/src/app/views/lista-livros/lista-livros.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { LivroService } from 'src/app/service/livro.service';
 
 @Component({
@@ -6,17 +7,27 @@ import { LivroService } from 'src/app/service/livro.service';
   templateUrl: './lista-livros.component.html',
   styleUrls: ['./lista-livros.component.css']
 })
-export class ListaLivrosComponent {
+export class ListaLivrosComponent implements OnDestroy {
 
   listaLivros: [];
   campoBusca: string = ''
+  subscription: Subscription
 
-  constructor(private service:LivroService) { }
+  constructor(private service: LivroService) { }
 
-buscarLivros(){
-  this.service.buscar(this.campoBusca)
-}
-
+  // todo service retorna um observable afim de gastar menos recursos é normal usar o metodo unsubscribe para isso, para
+  // cancelar a assinatura do metodo de busca na API
+  buscarLivros() {
+    this.service.buscar(this.campoBusca).subscribe({
+      next: retornoAPi => console.log(retornoAPi),
+      error: erro => console.error(erro),
+      complete: () => console.log("completado")
+    })
+  }
+  // cancela as execuções do observable
+  ngOnDestroy() {
+    this.subscription.unsubscribe()
+  }
 }
 
 
