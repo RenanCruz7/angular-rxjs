918b9faa7d4acd104a578a2b14fd2fabbe79ad36
RenanCruz <renancruz199@gmail.com>
2024-01-27 17:20:21 -0300

    Mostrando total de itens e finalizando curso

--- angular-rxjs/src/app/views/lista-livros/lista-livros.component.ts
+++ angular-rxjs/src/app/views/lista-livros/lista-livros.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { catchError, debounceTime, filter, map, switchMap, throwError } from 'rxjs';
-import { Item } from 'src/app/models/interfaces';
+import { catchError, debounceTime, filter, map, of, switchMap, throwError } from 'rxjs';
+import { Item, LivrosResultado } from 'src/app/models/interfaces';
 import { LivroVolumeInfo } from 'src/app/models/livroVolumeInfo';
 import { LivroService } from 'src/app/service/livro.service';
 
@@ -15,13 +15,27 @@ export class ListaLivrosComponent{
 
   campoBusca = new FormControl()
   mensagemErro = ''
+  livrosResultado: LivrosResultado
 
   constructor(private service: LivroService) { }
+
+  totalDeLivros$ = this.campoBusca.valueChanges.pipe(
+    debounceTime(PAUSA), // delay para que a requisição seja feita
+    filter((valorDigitado) => valorDigitado.length >= 3), //aqui a requisição so sera feita quando o valor digitador ser igual ou maior que 3 char 
+    switchMap((valorDigitado) => this.service.buscar(valorDigitado)),
+    map(resultado => this.livrosResultado = resultado),
+    catchError(erro =>{
+      console.log(erro)
+      return of()
+    })
+  )
+
 
   livrosEncontrador$ = this.campoBusca.valueChanges.pipe(
     debounceTime(PAUSA), // delay para que a requisição seja feita
     filter((valorDigitado) => valorDigitado.length >= 3), //aqui a requisição so sera feita quando o valor digitador ser igual ou maior que 3 char 
     switchMap((valorDigitado) => this.service.buscar(valorDigitado)),
+    map(resultado => resultado.items ?? []),
     map((items) =>this.livrosResultadoParaLivros(items)),
     catchError(erro =>{
       return throwError(()=> new Error(this.mensagemErro ='Ops, Ocorreu um erro'))
