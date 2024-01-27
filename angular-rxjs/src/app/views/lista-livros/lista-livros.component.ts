9c097c4914e3099a0312bb9b390d69c74cb89a82
RenanCruz <renancruz199@gmail.com>
2024-01-27 16:48:09 -0300

    Otimizando Desempenho

--- angular-rxjs/src/app/views/lista-livros/lista-livros.component.ts
+++ angular-rxjs/src/app/views/lista-livros/lista-livros.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnDestroy } from '@angular/core';
-import { Subscription } from 'rxjs';
-import { Item, Livro } from 'src/app/models/interfaces';
+import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { filter, map, switchMap } from 'rxjs';
+import { Item } from 'src/app/models/interfaces';
 import { LivroVolumeInfo } from 'src/app/models/livroVolumeInfo';
 import { LivroService } from 'src/app/service/livro.service';
 
@@ -9,35 +10,22 @@ import { LivroService } from 'src/app/service/livro.service';
   templateUrl: './lista-livros.component.html',
   styleUrls: ['./lista-livros.component.css']
 })
-export class ListaLivrosComponent implements OnDestroy {
+export class ListaLivrosComponent{
 
-  listaLivros: Livro[];
-  campoBusca: string = ''
-  subscription: Subscription
-  livro: Livro
+  campoBusca = new FormControl()
 
   constructor(private service: LivroService) { }
 
-  // todo service retorna um observable afim de gastar menos recursos é normal usar o metodo unsubscribe para isso, para
-  // cancelar a assinatura do metodo de busca na API
-  buscarLivros() {
-    this.service.buscar(this.campoBusca).subscribe({
-      next: (items) => this.listaLivros = this.livrosResultadoParaLivros(items),
-      error: erro => console.error(erro),
-    })
-  }
-
+  livrosEncontrador$ = this.campoBusca.valueChanges.pipe(
+    filter((valorDigitado) => valorDigitado.length >= 3),
+    switchMap((valorDigitado) => this.service.buscar(valorDigitado)),
+    map((items) =>this.livrosResultadoParaLivros(items)),
+  )
 
   livrosResultadoParaLivros(items: Item[]): LivroVolumeInfo[] {
     return items.map(item =>{
       return new LivroVolumeInfo(item)
     })
-  }
-
-
-  // cancela as execuções do observable
-  ngOnDestroy() {
-    this.subscription.unsubscribe()
   }
 }
 
